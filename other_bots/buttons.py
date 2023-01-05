5ca54d41f2e7afc4f9cf278d4e66fcba6fd34854
Maksemqwe123 <101874489+Maksemqwe123@users.noreply.github.com>
2023-01-05 13:21:32 +0300

    Delete other_bots directory

--- other_bots/buttons.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from parser_films_kinogo_life2 import *
-
-user_kb = InlineKeyboardMarkup(resize_keybord=True).add(
-    InlineKeyboardButton('Получить фильм', callback_data='films_buttons')
-)
